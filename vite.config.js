6e62162cc31fd2c8f99a11282f5aa7cb3fd68121
KwadwoDanso <Kwadwod00@gmail.com>
2026-05-12 15:34:26 -0400

    Add package config and HTML entry

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    // Allow importing relative files without writing .jsx every time
+    extensions: [".js", ".jsx", ".json"],
+  },
+});
